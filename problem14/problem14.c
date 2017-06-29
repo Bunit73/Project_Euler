53ceb5b52e146905302c6751e954f30aeee15b3d
Benjamin Ford <fordben@flip2.engr.oregonstate.edu>
2017-06-29 10:48:10 -0700

    problem 14

--- problem14/problem14.c
+++ problem14/problem14.c
@@ -5,28 +5,50 @@
 #include <stdio.h>
 #include <omp.h>
 
+#define limit 10000000
+
+int getSeq(int);
+
 int main(int argc, char *argv[]){
 	int maxValue = 0;
+	int longest = 0;
 	int i = 0;
+	double time0, time1, timeTaken;
+	time0 = omp_get_wtime();
 	
 	omp_set_num_threads( 8 );
 	
-	#pragma omp for 	
-    for(i = 1000000; i > 0; i--)
-    {
-       printf("%d ", i);
-    }
-	
-	maxValue = getSeq(13);
-	
-	printf("%d\n",maxValue);
-	
+	#pragma omp parallel default(none), shared(maxValue, longest), private(i)
+	{
+		#pragma omp for schedule(static)
+		for(i = limit; i > 0; i--)
+		{
+			int tempVal = 0;
+			int tid;
+			tempVal = getSeq(i);
+			tid = omp_get_thread_num();
+			#pragma omp critical
+			{
+				if(tempVal > maxValue){
+					maxValue = tempVal;
+					longest = i;
+				}
+			}
+		}
+	}
+	time1 = omp_get_wtime();
+	timeTaken = time1 - time0;
+	printf("Longest: %d Time Taken: %8.8lf\n",longest, timeTaken);
+ 	
 	return 0;
 }
 
-int getSeq(num){
+int getSeq(int num){
 	int ctr = 1;
 	long val = num;
+	if(val < 1){
+		return 0;
+	}
 	
 	while(val != 1){
 		
